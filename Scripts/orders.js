311be37b064150119cfa4d8af61636975be4c534
Saviour Eking <saviour.eking@outlook.com>
2023-07-07 02:10:47 +0100

    add order.js
    this file acts as an api and adds dynamic order details to the html markup then the browser parses it and shows on the page

---
+++ Scripts/orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: 'B5743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: 'B5743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '34652',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
